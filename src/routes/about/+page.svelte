553baa8d482d202b48adb8ed50af348ea97b9945
Stage <stage@sistema3.it>
2025-05-27 09:40:49 +0200

    e

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,25 +1,51 @@
-<svelte:head>
-	<title>calendario</title>
-	<meta name="description" content="Calendario Torneo Volley S3" />
-</svelte:head>
+<script>
+	// components behind the application
+	import Header from './Header.svelte';
+	import Controls from './Controls.svelte';
+	import Bracket from './Bracket.svelte';
+	
+	// store describing the teams and the shuffle function
+	import { names } from './stores.js';
+	// following the shuffle event shuffle the teams with the function provided on the store
+	function handleShuffle() {
+		names.shuffle();
+	}
+	
+	// based on the array of names, create an array of objects for the bracket
+	// this to provide an identifier as to distinguish the elements in the {#each} block
+	$: bracket = $names.map(name => ({
+		name,
+		id: Math.random()
+	}));
+</script>
 
-<div class="text-column">
-	<h1>Calendario torneo</h1>
+<style>
+	:global(*) {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+	:global(body) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	:global(body > * + *) {
+		margin-top: 1rem;
+	}
+	
+	.bracket {
+		max-width: 800px;
+		width: 100%;
+		display: flex;
+	}
+</style>
 
-	<p>
-		prossimamente!!!
-	</p>
+<Header title="Who's going to win?"/>
 
-	<pre>lavori in corso</pre>
+<Controls on:shuffle={handleShuffle}/>
 
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
-</div>
+<!-- to display the names side by side, wrap the bracket component in a container styled as a flex row -->
+<main class="bracket">
+	<Bracket {bracket} />
+</main>
