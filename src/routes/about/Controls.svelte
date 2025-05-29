823bede68187f93f22be8614ccadfb8b9bc715d1
Stage <stage@sistema3.it>
2025-05-29 10:05:03 +0200

    codice inutile perche non funziona

--- src/routes/about/Controls.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { createEventDispatcher } from 'svelte';
-	const dispatch = createEventDispatcher();
-</script>
-
-<button on:click={() => dispatch('shuffle')}>
-	Shuffle team order
-</button>
