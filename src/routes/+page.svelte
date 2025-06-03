750a26b2d21a5d28e21ffe5a4d7604fbdaa662c5
Stage <stage@sistema3.it>
2025-06-03 16:45:33 +0200

    vdv

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,18 +9,10 @@
 	let phone = '';
 
 	function handleSubmit() {
-		const current = get(registeredTeams);
-
-		if (current.length >= 4) {
-			alert('Il numero massimo di 4 squadre è stato raggiunto.');
-			return;
-		}
-
 		const newTeam: Team = { teamName, category, coachName, email, phone };
 		registeredTeams.update(teams => [...teams, newTeam]);
 
 		alert('Registrazione inviata con successo!');
-
 		// reset campi
 		teamName = '';
 		category = '';
