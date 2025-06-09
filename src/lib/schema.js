537898d0a4afc6cb0e59de19bc499be6d6b65717
Stage <stage@sistema3.it>
2025-06-09 10:46:12 +0200

    fd

--- src/lib/schema.js
+++ src/lib/schema.js
@@ -1,4 +1,5 @@
-import { mysqlTable, int, varchar, timestamp, sql } from 'drizzle-orm/mysql-core';
+import { mysqlTable, int, varchar, timestamp } from 'drizzle-orm/mysql-core';
+import { sql } from 'drizzle-orm';
 
 // Tabella utenti
 export const users = mysqlTable('users', {
